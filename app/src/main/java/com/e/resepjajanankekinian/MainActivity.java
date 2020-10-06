29b8befac57f31dc2794808056c20b37f58ab919
faqihuddinalf <macbook@faqihuddinalf.local>
2020-10-06 22:30:23 +0700

    Sprint 3

--- app/src/main/java/com/e/resepjajanankekinian/MainActivity.java
+++ app/src/main/java/com/e/resepjajanankekinian/MainActivity.java
@@ -2,7 +2,10 @@ package com.e.resepjajanankekinian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView showResep = (ImageView) findViewById(R.id.cheesecake);
+
+        showResep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, resep.class);
+                startActivity(intent);
+            }
+        });
     }
 }
