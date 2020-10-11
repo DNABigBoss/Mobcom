0d0ecaf5facfa15843896e720cc8baa4571ba5f4
faqihuddinalf <macbook@faqihuddinalf.local>
2020-10-11 20:55:31 +0700

    sprint4

---
+++ app/src/main/java/com/e/resepjajanankekinian/kulkas.java
@@ -0,0 +1,41 @@
+package com.e.resepjajanankekinian;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class kulkas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kulkas);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(kulkas.this, MainActivity.class));
+                        finish();
+                        break;
+                    case R.id.kulkas:
+                        break;
+                    case R.id.bookmark:
+                        break;
+                    case R.id.profile:
+                        startActivity(new Intent(kulkas.this, profil.class));
+                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
