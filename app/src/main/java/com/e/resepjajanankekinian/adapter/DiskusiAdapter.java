0829811301ea21cae1334f0640550fccdad24efe
DwikiSSaputra <37283373+DNABigBoss@users.noreply.github.com>
2020-11-10 19:55:14 +0700

    update sprint 8

--- app/src/main/java/com/e/resepjajanankekinian/adapter/DiskusiAdapter.java
+++ app/src/main/java/com/e/resepjajanankekinian/adapter/DiskusiAdapter.java
@@ -1,5 +1,6 @@
 package com.e.resepjajanankekinian.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -17,6 +18,11 @@ import com.e.resepjajanankekinian.model.DiskusiData;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -37,6 +43,7 @@ public class DiskusiAdapter extends RecyclerView.Adapter<DiskusiAdapter.CustomVi
 
         private final TextView textViewName;
         private final TextView textViewIsi;
+        private final TextView textViewTanggal;
         private final ImageView imageView;
         private final LinearLayout cardView;
 
@@ -47,6 +54,7 @@ public class DiskusiAdapter extends RecyclerView.Adapter<DiskusiAdapter.CustomVi
             textViewIsi = mView.findViewById(R.id.komenDiskusi);
             imageView = mView.findViewById(R.id.imageViewDiskusi);
             cardView = mView.findViewById(R.id.linearLayoutDiskusi);
+            textViewTanggal = mView.findViewById(R.id.tanggalDiskusi);
         }
     }
 
@@ -63,11 +71,37 @@ public class DiskusiAdapter extends RecyclerView.Adapter<DiskusiAdapter.CustomVi
         DiskusiData diskusiData = dataList.get(position);
         holder.textViewName.setText(diskusiData.getNama());
         holder.textViewIsi.setText(diskusiData.getIsi());
+        String tanggal = setTanggal(diskusiData.getTanggal());
+        holder.textViewTanggal.setText(tanggal);
 //        final Integer idx = diskusiData.getId();
     }
 
     @Override
     public int getItemCount() {
         return dataList.size();
+    }
+
+    private String setTanggal(String tanggalKomen) {
+        @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String tanggalSaatIni = dateFormat.format(Calendar.getInstance().getTime());
+        try {
+            Date d1 = dateFormat.parse(tanggalKomen);
+            Date d2 = dateFormat.parse(tanggalSaatIni);
+
+            long diff = d2.getTime() - d1.getTime();
+            long diffSeconds = diff / 1000 % 60;
+            long diffMinutes = diff / (60 * 1000) % 60;
+            long diffHours = diff / (60 * 60 * 1000) % 24;
+            long diffDays = diff / (24 * 60 * 60 * 1000);
+
+            if (diffDays >= 1) return String.format("%s hari",diffDays);
+            else if(diffHours >= 1) return String.format("%s jam",diffHours);
+            else if(diffMinutes >= 1) return String.format("%s menit",diffMinutes);
+            else if(diffSeconds >= 1) return String.format("%s detik",diffSeconds);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "null";
+        }
+        return "null";
     }
 }
