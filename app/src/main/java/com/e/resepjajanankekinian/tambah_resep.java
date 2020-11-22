a3f19e694ab1529409002dd7f4870cbb0f03afa9
faqihuddinalf <macbook@faqihuddinalf.local>
2020-11-22 22:22:39 +0700

    Membuat Layout List Resep Yang Telah Ditambahkan dan Membuat Layout Tambah Resep

---
+++ app/src/main/java/com/e/resepjajanankekinian/tambah_resep.java
@@ -0,0 +1,53 @@
+package com.e.resepjajanankekinian;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class tambah_resep extends AppCompatActivity {
+
+    AutoCompleteTextView textIn;
+    Button buttonAdd;
+    LinearLayout container;
+    TextView reList, info;
+
+    private static final String[] NUMBER = new String[] {
+            "One", "Two", "Three", "Four", "Five",
+            "Six", "Seven", "Eight", "Nine", "Ten"
+    };
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_resep);
+
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Tambah Resep Baru");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        //Set icon to toolbar
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    };
+
+}
