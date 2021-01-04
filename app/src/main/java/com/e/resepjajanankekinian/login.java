69e15153effad6a4ab1d8bffcb095eca216cddeb
DwikiSSaputra <37283373+DNABigBoss@users.noreply.github.com>
2021-01-04 19:26:54 +0700

    Update fix version 1.4.0

--- app/src/main/java/com/e/resepjajanankekinian/login.java
+++ app/src/main/java/com/e/resepjajanankekinian/login.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,9 @@ import com.e.resepjajanankekinian.model.UserData;
 import com.e.resepjajanankekinian.service.ApiClient;
 import com.e.resepjajanankekinian.service.ApiRequest;
 import com.e.resepjajanankekinian.service.SessionManager;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
 import java.util.List;
 
@@ -23,12 +27,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class login extends AppCompatActivity {
+public class login extends AppCompatActivity implements Validator.ValidationListener {
 
-    private EditText editText;
-    private EditText pass;
+    @NotEmpty private EditText editText;
+    @NotEmpty private EditText pass;
     ProgressDialog progressDialog;
     SessionManager sessionManager;
+    Validator validator;
+    private static final String TAG = "login";
+    ApiRequest apiRequest = ApiClient.getRetrofitInstance().create(ApiRequest.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,9 @@ public class login extends AppCompatActivity {
         editText = findViewById(R.id.editTextLogin);
         pass = findViewById(R.id.loginPass);
 
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,59 +66,12 @@ public class login extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String txtLogin = editText.getText().toString().trim();
-                String txtPass = pass.getText().toString().trim();
-                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
-                if(txtLogin != null && txtPass != null) {
-                    progressDialog = new ProgressDialog(login.this);
-                    progressDialog.setMessage("Loading....");
-                    progressDialog.show();
-                    if(txtLogin.matches(emailPattern)) {
-                        ApiRequest apiRequest = ApiClient.getRetrofitInstance().create(ApiRequest.class);
-                        Call<List<UserData>> call = apiRequest.getUser(null, null, txtLogin, txtPass);
-                        call.enqueue(new Callback<List<UserData>>() {
-                            @Override
-                            public void onResponse(@NonNull Call<List<UserData>> call, @NonNull Response<List<UserData>> response) {
-                                progressDialog.dismiss();
-                                String code = String.valueOf(response.code());
-                                if ("200".equals(code)) {
-                                    openMain(response.body());
-                                } else {
-                                    Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(@NonNull Call<List<UserData>> call, @NonNull Throwable t) {
-                                progressDialog.dismiss();
-                                Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        ApiRequest apiRequest = ApiClient.getRetrofitInstance().create(ApiRequest.class);
-                        Call<List<UserData>> call = apiRequest.getUser(null, txtLogin, null, txtPass);
-                        call.enqueue(new Callback<List<UserData>>() {
-                            @Override
-                            public void onResponse(@NonNull Call<List<UserData>> call, @NonNull Response<List<UserData>> response) {
-                                progressDialog.dismiss();
-                                String code = String.valueOf(response.code());
-                                if ("200".equals(code)) {
-                                    openMain(response.body());
-                                } else {
-                                    Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(@NonNull Call<List<UserData>> call, @NonNull Throwable t) {
-                                progressDialog.dismiss();
-                                Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                } else {
-                    Toast.makeText(login.this, "Tidak boleh dikosongkan", Toast.LENGTH_SHORT).show();
+                try {
+                    validator.validate();
+                } catch (Exception e) {
+                    // This will catch any exception, because they are all descended from Exception
+                    String message = "Error " + e.getMessage();
+                    Log.e(TAG, message);
                 }
             }
         });
@@ -129,5 +92,50 @@ public class login extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        progressDialog = new ProgressDialog(login.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        String txtLogin = editText.getText().toString().trim();
+        String txtPass = pass.getText().toString().trim();
+
+        Call<List<UserData>> call = apiRequest.getUser(null, txtLogin, null, txtPass);
+        call.enqueue(new Callback<List<UserData>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<UserData>> call, @NonNull Response<List<UserData>> response) {
+                progressDialog.dismiss();
+                String code = String.valueOf(response.code());
+                if ("200".equals(code)) {
+                    openMain(response.body());
+                } else {
+                    Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<UserData>> call, @NonNull Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(login.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
